4ec30cc2a0122bceded4b4135c3d53b8a7302d9a
Juan Ignacio Ubeira <jubeira@ekumenlabs.com>
2018-11-13 15:15:29 -0300

    Fix to make poco work with armv7 and armv8 according to flag.

--- config.sh
+++ config.sh
@@ -1,6 +1,13 @@
 system=$(uname -s | tr 'DL' 'dl')-$(uname -m)
 toolchain=llvm
 abi=arm64-v8a
+
+if [ "armeabi-v7a" = $abi ]; then
+    arch="arm"
+elif [ "arm64-v8a" = $abi ]; then
+    arch="arm64"
+fi
+
 platform=android-24
 PYTHONPATH=/opt/ros/kinetic/lib/python2.7/dist-packages:$PYTHONPATH
 # Enable this value for debug build
@@ -9,4 +16,3 @@ CMAKE_BUILD_TYPE=Release
 # Enable this if you need to use pluginlib in Android.
 # The plugins will be statically linked
 use_pluginlib=1
-
