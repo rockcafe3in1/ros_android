582db04c42f3cccfce7b397315f051eb80658228
ivanpauno <ivanpauno@ekumenlabs.com>
2018-11-26 13:01:42 -0300

    Added image_proc, depth_image_proc and stereo_image_proc. Solved transitive linking problem with bondcpp.

--- do_everything.sh
+++ do_everything.sh
@@ -202,6 +202,9 @@ if [[ $skip -ne 1 ]] ; then
 
     ## ROS patches
 
+    # Patch bondcpp - Fix transitive linking problems
+    apply_patch $my_loc/patches/bondcpp.patch
+
     # Patch opencv - Fix installation path
     apply_patch $my_loc/patches/opencv.patch
     
