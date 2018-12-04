100f3fc201c8546c85de139402de81f5e6867f38
Johannes Meyer <johannes@intermodalics.eu>
2018-12-04 14:20:17 +0100

    Upgrade lz4 to r131 (xenial version)
    r124 does not support aarch64, probably due to the CPU feature detection at
    https://github.com/lz4/lz4/compare/r124...r131#diff-a4dc0fa3ccaea8a4219c81928cba271aL49.

--- get_library.sh
+++ get_library.sh
@@ -52,7 +52,7 @@ elif [ $1 == 'libxml2' ]; then
     URL=ftp://xmlsoft.org/libxml2/libxml2-2.9.7.tar.gz
     COMP='gz'
 elif [ $1 == 'lz4' ]; then
-    URL=https://github.com/Cyan4973/lz4/archive/r124.tar.gz
+    URL=https://github.com/Cyan4973/lz4/archive/r131.tar.gz
     COMP='gz'
 elif [ $1 == 'pcl' ]; then
     URL=https://github.com/PointCloudLibrary/pcl/archive/pcl-1.8.1.tar.gz
