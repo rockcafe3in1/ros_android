2fdb1344f6611aeea5b683d9427b31633eca657e
Juan Ignacio Ubeira <jubeira@ekumenlabs.com>
2019-01-07 11:40:21 -0300

    Renaming dockerized entrypoint.

--- dockerized_install.sh
+++ dockerized_install.sh
@@ -6,4 +6,4 @@ my_loc="$(cd "$(dirname $0)" && pwd)"
 source $SCRIPT_DIR/utils.sh
 
 $my_loc/docker/build.sh
-$my_loc/docker/run.sh $my_loc/do_everything.sh $my_loc/output
+$my_loc/docker/run.sh $my_loc/install.sh $my_loc/output
