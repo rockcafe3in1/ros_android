a0ce17ccd3d43488a7f405cb3954d9d6ec92503d
Juan Ignacio Ubeira <jubeira@ekumenlabs.com>
2019-01-08 10:32:30 -0300

    Pluginlib support always enabled; removing flag.

--- do_everything.sh
+++ do_everything.sh
@@ -119,29 +119,27 @@ fi
 
 # Before build
 # Search packages that depend on pluginlib and generate plugin loader.
-if [ $use_pluginlib -ne 0 ]; then
-    echo
-    echo -e '\e[34mBuilding pluginlib support...\e[39m'
-    echo
+echo
+echo -e '\e[34mBuilding pluginlib support...\e[39m'
+echo
 
-    pluginlib_helper_file=pluginlib_helper.cpp
-    $my_loc/files/pluginlib_helper/pluginlib_helper.py -scanroot $prefix/catkin_ws/src $user_workspace -cppout $my_loc/files/pluginlib_helper/$pluginlib_helper_file
-    cp $my_loc/files/pluginlib_helper/$pluginlib_helper_file $prefix/catkin_ws/src/pluginlib/src/
-    line="add_library(pluginlib STATIC src/pluginlib_helper.cpp)"
-    # temporally turn off error detection
-    set +e
-    grep "$line" $prefix/catkin_ws/src/pluginlib/CMakeLists.txt
-    # if line is not already added, then add it to the pluginlib cmake
-    if [ $? -ne 0 ]; then
-        # backup the file
-        cp $prefix/catkin_ws/src/pluginlib/CMakeLists.txt $prefix/catkin_ws/src/pluginlib/CMakeLists.txt.bak
-        sed -i '/INCLUDE_DIRS include/a LIBRARIES ${PROJECT_NAME}' $prefix/catkin_ws/src/pluginlib/CMakeLists.txt
-        echo -e "\n"$line >> $prefix/catkin_ws/src/pluginlib/CMakeLists.txt
-        echo 'install(TARGETS pluginlib RUNTIME DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION} ARCHIVE DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION} LIBRARY DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION})' >> $prefix/catkin_ws/src/pluginlib/CMakeLists.txt
-    fi
-    # turn error detection back on
-    set -e
+pluginlib_helper_file=pluginlib_helper.cpp
+$my_loc/files/pluginlib_helper/pluginlib_helper.py -scanroot $prefix/catkin_ws/src $user_workspace -cppout $my_loc/files/pluginlib_helper/$pluginlib_helper_file
+cp $my_loc/files/pluginlib_helper/$pluginlib_helper_file $prefix/catkin_ws/src/pluginlib/src/
+line="add_library(pluginlib STATIC src/pluginlib_helper.cpp)"
+# temporally turn off error detection
+set +e
+grep "$line" $prefix/catkin_ws/src/pluginlib/CMakeLists.txt
+# if line is not already added, then add it to the pluginlib cmake
+if [ $? -ne 0 ]; then
+    # backup the file
+    cp $prefix/catkin_ws/src/pluginlib/CMakeLists.txt $prefix/catkin_ws/src/pluginlib/CMakeLists.txt.bak
+    sed -i '/INCLUDE_DIRS include/a LIBRARIES ${PROJECT_NAME}' $prefix/catkin_ws/src/pluginlib/CMakeLists.txt
+    echo -e "\n"$line >> $prefix/catkin_ws/src/pluginlib/CMakeLists.txt
+    echo 'install(TARGETS pluginlib RUNTIME DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION} ARCHIVE DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION} LIBRARY DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION})' >> $prefix/catkin_ws/src/pluginlib/CMakeLists.txt
 fi
+# turn error detection back on
+set -e
 
 echo
 echo -e '\e[34mBuilding library dependencies.\e[39m'
