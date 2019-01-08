c3063a553df90128ac1207316a727c9bf0edea12
Ivan Paunovic <ivanpauno@ekumenlabs.com>
2019-01-08 17:20:43 -0300

    Solved issue with dockerized install script

--- docker/run.sh
+++ docker/run.sh
@@ -5,13 +5,13 @@ REPO_ROOT=$SCRIPTPATH/../
 IMAGE=android_ndk
 
 if [ "$#" == 0 ]; then
-  EXTRA_ARGS="bash"
+  EXTRA_ARGS=(bash)
 else
-  EXTRA_ARGS="bash -c \"$@\""
+  EXTRA_ARGS=(bash -c "$*")
 fi
 
 docker run \
   -v ${REPO_ROOT}:/opt/ros_android \
   --privileged \
   -it \
-  ${IMAGE} "${EXTRA_ARGS}"
+  ${IMAGE} "${EXTRA_ARGS[@]}"
