c784b53e52d6c1c5054e7eceece5c21215791af5
Juan Ignacio Ubeira <jubeira@ekumenlabs.com>
2019-01-10 14:10:48 -0300

    Fix for toolchain flags.

--- android.toolchain.cmake
+++ android.toolchain.cmake
@@ -4,10 +4,13 @@ if(ANDROID_NDK_TOOLCHAIN_INCLUDED)
 endif(ANDROID_NDK_TOOLCHAIN_INCLUDED)
 include($ENV{ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake)
 
-# Debug and release flags.
-list(REMOVE_ITEM ANDROID_COMPILER_FLAGS -g)
-list(APPEND ANDROID_COMPILER_FLAGS_DEBUG -g)
-list(APPEND ANDROID_COMPILER_FLAGS_RELWITHDEBINFO -g ${ANDROID_COMPILER_FLAGS_RELEASE})
+## Build without debug symbols in Release mode and add RelWithDebInfo mode.
+foreach(_var ANDROID_COMPILER_FLAGS CMAKE_C_FLAGS CMAKE_CXX_FLAGS CMAKE_ASM_FLAGS)
+  string(REPLACE "-g " "" ${_var} "${${_var}}")
+  set(${_var}_DEBUG "-g ${${_var}_DEBUG}")
+  set(${_var}_RELWITHDEBINFO "-g ${${_var}_RELEASE}")
+endforeach()
+unset(_var)
 
 if(ANDROID_TOOLCHAIN STREQUAL clang)
   list(APPEND ANDROID_COMPILER_FLAGS_RELWITHDEBINFO -fno-limit-debug-info)
