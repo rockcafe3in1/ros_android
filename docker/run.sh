684bdc860456b632e7288e2ccd8970950f237b6c
Johannes Meyer <johannes@intermodalics.eu>
2019-01-17 00:55:10 +0100

    docker: split command line at double dash and forward remainings arguments to docker run as a command

--- docker/run.sh
+++ docker/run.sh
@@ -4,9 +4,34 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 REPO_ROOT=$SCRIPTPATH/../
 IMAGE=android_ndk
 
+DOCKEROPTS=()
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --help)
+      # print usage
+      echo "Usage: $0 [DOCKER OPTIONS] [-- [COMMAND]]"
+      echo
+      echo "Docker options:"
+      docker run --help | tail -n +7
+      exit 0
+      ;;
+    --)
+      # end-of-options: all arguments after the -- will be interpreted as a command to run inside the container.
+      shift
+      break
+      ;;
+    *)
+      # collect docker options
+      DOCKEROPTS+=("$1")
+      shift
+      ;;
+  esac
+done
+
+set -x
 docker run \
   -v ${REPO_ROOT}:/opt/ros_android \
   --privileged \
   -it \
-  "$@" \
-  ${IMAGE} bash
+  "${DOCKEROPTS[@]}" \
+  ${IMAGE} "$@"
