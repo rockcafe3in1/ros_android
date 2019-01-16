0604ee523e790a367fd79c2c78dca552b5c1015a
Johannes Meyer <johannes@intermodalics.eu>
2019-01-16 19:38:31 +0100

    docker/run.sh: apply extra arguments to docker run instead of bash
    This is needed to mount extra directories from the host, e.g. for the
    `do_everything.sh --extends-workspace` feature.

--- docker/run.sh
+++ docker/run.sh
@@ -4,14 +4,9 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 REPO_ROOT=$SCRIPTPATH/../
 IMAGE=android_ndk
 
-if [ "$#" == 0 ]; then
-  EXTRA_ARGS="bash"
-else
-  EXTRA_ARGS="bash -c \"$@\""
-fi
-
 docker run \
   -v ${REPO_ROOT}:/opt/ros_android \
   --privileged \
   -it \
-  ${IMAGE} "${EXTRA_ARGS}"
+  "$@" \
+  ${IMAGE} bash
